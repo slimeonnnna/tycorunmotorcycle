f14ae43f977620e052d88e4c9a8eaff657502f5e
slimeon <slimeonnnna@gmail.com>
2026-01-22 11:48:55 +0800

    update

--- lib/site-url.ts
+++ lib/site-url.ts
@@ -3,6 +3,10 @@ export function getBaseUrl() {
     return process.env.NEXT_PUBLIC_SITE_URL;
   }
 
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  }
+
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
